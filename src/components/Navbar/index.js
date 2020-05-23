ebf756fd5737be8387c5debb6936c5ccac1d8165
Thales Ludwig <thales.ludwig@gmail.com>
2020-05-23 12:29:50 -0300

    Added Navbar condition

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from "react-router";
 import { faBell, faHome, faEnvelope, faSearch } from '@fortawesome/free-solid-svg-icons'
 import {
     NavbarStyled,
@@ -16,48 +17,59 @@ import {
     SearchIcon,
 } from './NavbarStyled'
 
-const Navbar = props => (
-    <NavbarStyled theme={props.theme} elevation={props.elevation}>
-        <Wrapper>
-            <BrandSearchWrapper>
-                <LinkStyled to="/">
-                    <Brand theme={props.theme}>Portfolio</Brand>
-                </LinkStyled>
-                <SearchWrapper>
-                    <SearchIcon icon={faSearch} size="lg" theme={props.theme} />
-                    <SearchInput theme={props.theme} placeholder="search..." />
-                </SearchWrapper>
-            </BrandSearchWrapper>
-            <Buttons>
-                <LinkStyled to="/">
-                    <Icon icon={faHome} size="lg" theme={props.theme} />
-                </LinkStyled>
-                <LinkSearch to="/">
-                    <Icon icon={faSearch} size="lg" theme={props.theme} />
-                </LinkSearch>
-                <LinkStyled to="/">
-                    <Icon icon={faEnvelope} size="lg" theme={props.theme} />
-                </LinkStyled>
-                <LinkStyled to="/">
-                    <Icon icon={faBell} size="lg" theme={props.theme} />
-                </LinkStyled>
-                <LinkStyled to="/profile">
-                    <ProfileImage theme={props.theme} image={props.user.profileImage.url} />
-                </LinkStyled>
-            </Buttons>
-        </Wrapper>
-    </NavbarStyled>
-)
+const Navbar = props => {
+
+    return shouldRender(props.location.pathname) ?
+        (
+        <NavbarStyled theme={props.theme} elevation={props.elevation}>
+            <Wrapper>
+                <BrandSearchWrapper>
+                    <LinkStyled to="/">
+                        <Brand theme={props.theme}>Portfolio</Brand>
+                    </LinkStyled>
+                    <SearchWrapper>
+                        <SearchIcon icon={faSearch} size="lg" theme={props.theme} />
+                        <SearchInput theme={props.theme} placeholder="search..." />
+                    </SearchWrapper>
+                </BrandSearchWrapper>
+                <Buttons>
+                    <LinkStyled to="/">
+                        <Icon icon={faHome} size="lg" theme={props.theme} />
+                    </LinkStyled>
+                    <LinkSearch to="/">
+                        <Icon icon={faSearch} size="lg" theme={props.theme} />
+                    </LinkSearch>
+                    <LinkStyled to="/">
+                        <Icon icon={faEnvelope} size="lg" theme={props.theme} />
+                    </LinkStyled>
+                    <LinkStyled to="/">
+                        <Icon icon={faBell} size="lg" theme={props.theme} />
+                    </LinkStyled>
+                    <LinkStyled to="/profile">
+                        <ProfileImage theme={props.theme} image={props.user.profileImage.url} />
+                    </LinkStyled>
+                </Buttons>
+            </Wrapper>
+        </NavbarStyled>
+        )
+        :
+        <React.Fragment />
+}
+
+const shouldRender = activeRoute => (activeRoute !== '/new-panel')
 
 Navbar.propTypes = {
     theme: PropTypes.object.isRequired,
     elevation: PropTypes.string,
     user: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
 }
 
 Navbar.defaultProps = {
     elevation: '',
 }
 
-export default Navbar
+export default withRouter(Navbar)
 
