451bd1264893e54188a5a8b600bce794d5998ec2
Thales Ludwig <thales.ludwig@gmail.com>
2020-06-19 15:44:59 -0300

    Added minified profile (#2)

--- src/components/ProfileCard/ProfileCardStyled.js
+++ src/components/ProfileCard/ProfileCardStyled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import TYPOGRAPHY from '../../constants/typography'
+import PROFILE from '../../constants/profile'
 
 export const ProfileCardStyled = styled.div`
     background: url(${({ image }) => image}) no-repeat center;
@@ -13,7 +15,19 @@ export const Gradient = styled.div`
     background-image: ${({theme}) => `linear-gradient(transparent, ${theme.BACKGROUND})`};
     display: flex;
     flex-direction: column;
-    align-items: ${({ model }) => model === 2 || model === 4 ? 'flex-start' : 'center'};
+    align-items: ${({ model }) => {
+        switch (model) {
+            case PROFILE.PROFILE_STYLES.DEFAULT_LEFT_ALIGNED:
+            case PROFILE.PROFILE_STYLES.COVER_ONLY_LEFT_ALIGNED:
+            case PROFILE.PROFILE_STYLES.MINIFIED:
+                return 'flex-start'
+            case PROFILE.PROFILE_STYLES.DEFAULT_CENTER:
+            case PROFILE.PROFILE_STYLES.COVER_ONLY_CENTER:
+                return 'center'
+            default:
+                break
+        }
+    }};
     justify-content: flex-end;
     padding: 0px 20px;
     height: 480px;
@@ -29,22 +43,37 @@ export const ProfileImage = styled.div`
     margin-left: ${({ model }) => model === 2 || model === 4 ? '20px' : '0px'};
 `
 
+export const ProfileImageMini = styled.div`
+    background: url(${({ image }) => image}) no-repeat center;
+    background-size: cover;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    margin-bottom: 20px;
+    margin-left: 0px;
+`
+
 export const InfoWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: ${({ model }) => model === 2 || model === 4 ? 'flex-start' : 'center'};
-    margin-left: ${({ model }) => model === 2 || model === 4 ? '20px' : '0px'};
+    align-items: ${({ model }) => model === 2 || model === 4 || model === 5 ? 'flex-start' : 'center'};
+    margin-left: ${({ model }) => model === 2 || model === 4 || model === 5 ? '20px' : '0px'};
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
 `
 
 export const Name = styled.div`
     color: ${({theme}) => theme.TITLE};
-    font-size: ${({typography}) => typography.SIZE.TITLE};
+    font-size: ${TYPOGRAPHY.SIZE.TITLE};
     font-weight: 600;
 `
 
 export const Occupation = styled.div`
     color: ${({theme}) => theme.TEXT};
-    font-size: ${({typography}) => typography.SIZE.SUBTITLE};
+    font-size: ${TYPOGRAPHY.SIZE.SUBTITLE};
     margin-top: 10px;
 `
 
@@ -57,12 +86,12 @@ export const LikeRow = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    margin: 10px 0px;
+    margin: ${({ model }) => model !== PROFILE.PROFILE_STYLES.MINIFIED ? '10px 0px' : '0px'};
 `
 
 export const LikeCount = styled.div`
     color: ${({theme}) => theme.TEXT};
     font-weight: bold;
-    font-size: ${({typography}) => typography.SIZE.TEXT};
+    font-size: ${TYPOGRAPHY.SIZE.TEXT};
     margin-left: 10px;
 `
